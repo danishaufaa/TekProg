1c5e696d476ff214f68fca5a74e3be1c8104eefe
danishaufaa <muhammad.danish.tif424@polban.ac.id>
2025-02-10 15:07:27 +0700

    Add files via upload

---
+++ welcome.java
@@ -0,0 +1,16 @@
+/**
+ * This program displays a greeting for for the reader
+ * @version 1.1 2025-02-10
+ * @author Muhammad Danish Aufa
+ */
+public class welcome 
+{
+    public static void main(String[] args)
+    {
+        String greeting = "Welcome to Core Java!";
+        System.out.println(greeting);
+        for (int i = 0; i < greeting.length(); i++)
+            System.out.print("=");
+        System.out.println();
+    }
+}
